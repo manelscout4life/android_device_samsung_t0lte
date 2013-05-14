3215bd9dfcae6814767fc5d8fc168f708844e298
Joao <simoesj90@gmail.com>
2013-05-13 23:42:51 -0500

    Should be fixed now
    Change-Id: I42562a3ff0175fbdd575bc4c688d1cb7ac9b88b7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 -include device/samsung/smdk4412-common/BoardCommonConfig.mk
-#-include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
+-include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
 
 -include device/samsung/t0lte/BoardCommonConfig.mk
 
